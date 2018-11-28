86833bc1eb541db4ca516a69d8db2c4dfc00f857
artemiy <artem.galanix@gmail.com>
2018-11-28 13:41:05 +0200

    v0.1.0 of chat

--- src/App.js
+++ src/App.js
@@ -1,14 +1,237 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import ExploreIcon from '@material-ui/icons/Explore';
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Input from '@material-ui/core/Input';
+import Avatar from '@material-ui/core/Avatar';
+
+import { chats, messages } from './mock-data'
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-    <h1>Chat</h1>
-      </div>
-    );
-  }
+
+const drawerWidth = 320;
+
+const styles = theme => ({
+  root: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    backgroundColor: theme.palette.background.default,
+  },
+  appBar: {
+    position: 'fixed',
+    width: `calc(100% - 320px)`,
+  },
+  drawerPaper: {
+    position: 'relative',
+    height: '100%',
+    width: 320,
+  },
+  drawerHeader: {
+    ...theme.mixins.toolbar,
+    paddingLeft: theme.spacing.unit * 3,
+    paddingRight: theme.spacing.unit * 3,
+  },
+  chatsList: {
+    height: 'calc(100% - 56px)',
+    overflowY: 'scroll',
+  },
+  newChatButton: {
+    position: 'absolute',
+    left: 'auto',
+    right: theme.spacing.unit * 3,
+    bottom: theme.spacing.unit * 3 + 48, // + bottom navigation
+  },
+  chatLayout: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: '64px',
+    height: '100%',
+    overflow: 'hidden',
+  },
+  messagesWrapper: {
+    overflowX: 'scroll',
+    height: '100%',
+    width: '100%',
+    paddingTop: theme.spacing.unit * 3,
+    paddingBottom: '120px',
+  },
+  messageInputWrapper: {
+    position: 'fixed',
+    left: 'auto',
+    right: 0,
+    bottom: 0,
+    width: `calc(100% - 320px)`,
+    padding: theme.spacing.unit * 3,
+  },
+  messageInput: {
+    padding: theme.spacing.unit * 2,
+  },
+  messageWrapper: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 3}px`,
+  },
+  messageWrappperFromMe: {
+    justifyContent: 'flex-end',
+  },
+  message: {
+    maxWidth: '70%',
+    minWidth: '10%',
+    padding: theme.spacing.unit,
+    marginLeft: theme.spacing.unit * 2,
+  },
+  messageFromMe: {
+    marginRight: theme.spacing.unit * 2,
+    backgroundColor: '#e6dcff'
+  },
+});
+
+function PermanentDrawerLeft(props) {
+  const { classes } = props;
+
+
+  
+      
+ 
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            React Chat
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+      
+        <div className={classes.drawerHeader}>
+            <TextField
+              fullWidth
+              margin="normal"
+              placeholder="Search chats..."
+            />
+          </div>
+          <Divider />
+
+
+
+          
+  
+         
+         <List className={classes.chatsList}>
+            {chats.map((chat, index) => (
+              <ListItem key={index} button>
+                <Avatar>{chat.title && chat.title[0]}</Avatar>
+                <ListItemText primary={chat.title}/>
+              </ListItem>
+            ))}
+          </List>
+         
+       
+     
+        
+
+
+
+       <Button
+            variant="fab"
+            color="primary"
+            className={classes.newChatButton}
+          >
+            <AddIcon />
+          </Button>
+          <BottomNavigation showLabels>
+            <BottomNavigationAction label="My Chats" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Explore" icon={<ExploreIcon />} />
+          </BottomNavigation>
+      
+        
+      </Drawer>
+      <main className={classes.chatLayout}>
+          <div className={classes.messagesWrapper}>
+            {messages && messages.map((message, index) => {
+              const isMessageFromMe = message.sender === 'me';
+
+              const userAvatar = (
+                <Avatar>
+                  {message.sender[0]}
+                </Avatar>
+              );
+
+              return (
+                // <div className={classNames(classes.messageWrapper, isMessageFromMe && classes.messageWrappperFromMe)}>
+                <div key={index} className={[
+                  classes.messageWrapper,
+                  isMessageFromMe ? classes.messageWrappperFromMe : ''
+                ].join(' ')}>
+                  {!isMessageFromMe && userAvatar}
+                  <Paper className={[
+                    classes.message,
+                    isMessageFromMe ? classes.messageFromMe : ''
+                  ].join(' ')}>
+                    <Typography variant="caption">
+                      {message.sender}
+                    </Typography>
+                    <Typography variant="body1">
+                      {message.content}
+                    </Typography>
+                  </Paper>
+                  {isMessageFromMe && userAvatar}
+                </div>
+              );
+            })}
+          </div>
+
+
+
+
+      
+      <div className={classes.messageInputWrapper}>
+            <Paper className={classes.messageInput} elevation={6}>
+            <TextField
+          id="standard-full-width"
+          label="Type message"
+          fullWidth
+        />
+            </Paper>
+     </div>
+
+     </main>
+     
+      
+     
+
+    </div>
+  );
+
 }
 
-export default App;
+export default withStyles(styles)(PermanentDrawerLeft);
